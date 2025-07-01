a862a8b1f7be54be048b19f7e10a62d1ed51baa8
Nguyen Minh Quan <minhhquann1508@gmail.com>
2025-07-01 14:16:26 +0700

    push

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,7 +12,7 @@ interface Config {
 }
 
 const config: Config = {
-  port: Number(process.env.PORT) || 3000,
+  port: Number(process.env.PORT) || 5000,
   nodeEnv: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/cinestar',
   JWT_SECRET: process.env.JWT_SECRET || '',
